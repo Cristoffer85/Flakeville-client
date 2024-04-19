5ab0075aefff3a0a36ee5331b94fda56771384cc
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-04-19 15:41:01 +0200

    Name refactoring for making functions and const more logical

--- sf-frontend/src/components/Router.jsx
+++ sf-frontend/src/components/Router.jsx
@@ -10,18 +10,20 @@ import UserAcc from "./UserAccount.jsx";
 import Navbar from './Navbar';
 
 function AppRouter({ isLoggedIn, handleLogin, handleLogout, username, showPopup, setShowPopup }) {
+    const commonProps = { isLoggedIn, handleLogin, handleLogout, username, showPopup, setShowPopup };
+
     return (
         <Router>
             <div>
-                <Navbar isLoggedIn={isLoggedIn} handleLogin={handleLogin} handleLogout={handleLogout} showPopup={showPopup} setShowPopup={setShowPopup} /> {}
+                <Navbar {...commonProps} />
                 <Routes>
-                    <Route path="/account" element={<Account isLoggedIn={isLoggedIn} handleLogin={handleLogin} />} />
-                    <Route path="/store" element={<Store />} />
-                    <Route path="/weather" element={<Weather />} />
-                    <Route path="/" element={<Home />} />
-                    <Route path="/admin" element={<AdminAcc />} />
-                    <Route path="/employee" element={<EmployeeAcc />} />
-                    <Route path="/user/:username" element={<UserAcc isLoggedIn={isLoggedIn} handleLogout={handleLogout} username={username} />} /> {}
+                    <Route path="/account" element={<Account {...commonProps} />} />
+                    <Route path="/store" element={<Store {...commonProps} />} />
+                    <Route path="/weather" element={<Weather {...commonProps} />} />
+                    <Route path="/" element={<Home {...commonProps} />} />
+                    <Route path="/admin" element={<AdminAcc {...commonProps} />} />
+                    <Route path="/employee" element={<EmployeeAcc {...commonProps} />} />
+                    <Route path="/user/:username" element={<UserAcc {...commonProps} />} />
                 </Routes>
             </div>
         </Router>
