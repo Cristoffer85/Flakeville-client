57917e5895031e56ba60300988d4f0493db1d7e4
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-04-24 14:08:33 +0200

    First fully step of Store is working!!! Yey! Can add products present in database and in separate controllers in to a cart in frontend, and make a "Send" order only if you are logged in as a user!! Cool huh!

--- sf-frontend/src/pages/Cart.jsx
+++ sf-frontend/src/pages/Cart.jsx
@@ -1,6 +1,40 @@
 import React, {useContext} from 'react';
 import CartContext from "../components/CartContext.jsx";
 import './css/Cart.css';
+import Cookies from 'js-cookie';
+
+async function sendOrder(cart) {
+    const token = Cookies.get('token'); // Get the token from cookies
+    const username = Cookies.get('username'); // Get the username from cookies
+
+    const order = {
+        id: "1", // You might want to generate this dynamically
+        products: cart.map(item => ({
+            product: {
+                id: item.id,
+                name: item.name,
+                description: item.description,
+                price: item.price
+            },
+            quantity: item.quantity // Assuming each item in the cart has a quantity property
+        }))
+    };
+
+    const response = await fetch(`http://localhost:8080/user/addOrder/${username}/orders`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(order)
+    });
+
+    if (response.ok) {
+        console.log('Order sent successfully');
+    } else {
+        console.log('Failed to send order:', await response.text());
+    }
+}
 
 function Cart() {
     const { cart } = useContext(CartContext);
@@ -20,6 +54,7 @@ function Cart() {
                     <p>{item.price}</p>
                 </div>
             ))}
+            <button onClick={() => sendOrder(cart)}>Send Order</button>
         </div>
     );
 }
