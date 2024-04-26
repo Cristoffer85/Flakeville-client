ea067fba36c66aad861420f5c9fe142039499bd1
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-04-26 09:56:00 +0200

    Create, update and deleteproduct endpoints working

--- sf-frontend/src/components/Products.jsx
+++ sf-frontend/src/components/Products.jsx
@@ -1,4 +1,63 @@
 import React, { useState } from 'react';
+import Cookies from "js-cookie";
+
+export const createProduct = async (product) => {
+    const token = Cookies.get('token');
+
+    const response = await fetch('http://localhost:8080/products/createProduct', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(product)
+    });
+
+    if (response.ok) {
+        console.log('Product created');
+        // You can add additional logic here, such as refreshing the product list
+    } else {
+        console.log('Product creation failed:', await response.text());
+    }
+};
+
+export const updateProduct = async (id, product) => {
+    const token = Cookies.get('token');
+
+    const response = await fetch(`http://localhost:8080/products/updateProduct/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(product)
+    });
+
+    if (response.ok) {
+        console.log('Product updated');
+        // You can add additional logic here, such as refreshing the product list
+    } else {
+        console.log('Product update failed:', await response.text());
+    }
+};
+
+export const deleteProduct = async (productId) => {
+    const token = Cookies.get('token'); // Get the token from cookies
+
+    const response = await fetch(`http://localhost:8080/products/deleteProduct/${productId}`, {
+        method: 'DELETE',
+        headers: {
+            'Authorization': `Bearer ${token}` // Include the token in the Authorization header
+        }
+    });
+
+    if (!response.ok) {
+        console.error('Error:', response.statusText);
+        return;
+    }
+
+    console.log('Product deleted successfully');
+};
 
 function Products({ product, addToCart }) {
     const [quantity, setQuantity] = useState(1);
--- sf-frontend/src/pages/EmployeeAccount.jsx
+++ sf-frontend/src/pages/EmployeeAccount.jsx
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from 'react';
 import {useNavigate} from "react-router-dom";
 import Cookies from 'js-cookie';
 import './css/EmployeeAccount.css';
+import { createProduct, updateProduct, deleteProduct } from '../components/Products.jsx';
 
 function Employee() {
     const username = Cookies.get('username');
     const [employeeDetails, setEmployeeDetails] = useState({name: '', position: ''});
     const [formFields, setFormFields] = useState({name: '', position: ''});
     const [successMessage, setSuccessMessage] = useState('');
+    const [createProductFormFields, setCreateProductFormFields] = useState({name: '', description: '', price: ''});
+    const [updateProductFormFields, setUpdateProductFormFields] = useState({Id: '', name: '', description: '', price: ''});
+    const [deleteProductId, setDeleteProductId] = useState('');
+    const [productId, setProductId] = useState('');
     useNavigate();
 
     useEffect(() => {
@@ -61,8 +66,26 @@ function Employee() {
         }
     };
 
+    const handleCreateProduct = async (event) => {
+        event.preventDefault();
+        await createProduct(createProductFormFields);
+        setCreateProductFormFields({name: '', description: '', price: ''}); // Clear the form
+    };
+
+    const handleUpdateProduct = async (event) => {
+        event.preventDefault();
+        await updateProduct(productId, updateProductFormFields);
+        setUpdateProductFormFields({Id: '', name: '', description: '', price: ''}); // Clear the form
+    };
+
+    const handleDeleteProduct = async (event) => {
+        event.preventDefault();
+        await deleteProduct(deleteProductId);
+        setDeleteProductId(''); // Clear the form
+    };
+
     return (
-        <>
+        <div className="employeeAccountContainer">
             <div className="updateEmployeeDetailsBox">
                 <p>Welcome, {username}!</p>
                 {employeeDetails && (
@@ -88,7 +111,67 @@ function Employee() {
                     <p>{successMessage}</p>
                 </form>
             </div>
-        </>
+            <div className="productManagementBox">
+                <form onSubmit={handleCreateProduct}>
+                    <h2>Create Product</h2>
+                    <div className="form-field">
+                        <label>Name:</label>
+                        <input type="text" value={createProductFormFields.name}
+                               onChange={e => setCreateProductFormFields({...createProductFormFields, name: e.target.value})}
+                               required/>
+                    </div>
+                    <div className="form-field">
+                        <label>Description:</label>
+                        <input type="text" value={createProductFormFields.description}
+                               onChange={e => setCreateProductFormFields({...createProductFormFields, description: e.target.value})}
+                               required/>
+                    </div>
+                    <div className="form-field">
+                        <label>Price:</label>
+                        <input type="number" value={createProductFormFields.price}
+                               onChange={e => setCreateProductFormFields({...createProductFormFields, price: e.target.value})}
+                               required/>
+                    </div>
+                    <button type="submit">Create</button>
+                </form>
+                <form onSubmit={handleUpdateProduct}>
+                    <h2>Update Product</h2>
+                    <div className="form-field">
+                        <label>ID:</label>
+                        <input type="text" value={productId}
+                               onChange={e => setProductId(e.target.value)} required/>
+                    </div>
+                    <div className="form-field">
+                        <label>Name:</label>
+                        <input type="text" value={updateProductFormFields.name}
+                               onChange={e => setUpdateProductFormFields({...updateProductFormFields, name: e.target.value})}
+                               required/>
+                    </div>
+                    <div className="form-field">
+                        <label>Description:</label>
+                        <input type="text" value={updateProductFormFields.description}
+                               onChange={e => setUpdateProductFormFields({...updateProductFormFields, description: e.target.value})}
+                               required/>
+                    </div>
+                    <div className="form-field">
+                        <label>Price:</label>
+                        <input type="number" value={updateProductFormFields.price}
+                               onChange={e => setUpdateProductFormFields({...updateProductFormFields, price: e.target.value})}
+                               required/>
+                    </div>
+                    <button type="submit">Update</button>
+                </form>
+                <form onSubmit={handleDeleteProduct}>
+                    <h2>Delete Product</h2>
+                    <div className="form-field">
+                        <label>ID:</label>
+                        <input type="text" value={deleteProductId}
+                               onChange={e => setDeleteProductId(e.target.value)} required/>
+                    </div>
+                    <button type="submit">Delete</button>
+                </form>
+            </div>
+        </div>
     );
 }
 
