84eda0f4a17e5d40d439c3f292aa31580a6b70ac
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-05-05 21:24:35 +0200

    Added some data for Home.jsx for first time and some more categories

--- sf-frontend/src/pages/Home.jsx
+++ sf-frontend/src/pages/Home.jsx
@@ -5,13 +5,21 @@ function Home() {
     return (
         <>
             <div className="box1">
-                <h2>Box Title</h2>
-                <p>Box content...</p>
+                <h2>Welcome!</h2>
+                <p>We welcome you to SnöFjällby. The metropol of both skiing - adventure, haven and utopia at
+                    the same time!</p>
+                <p>Feel free to navigate across our site and if youre lost, we guarantee you will find out where to go
+                    ahead next!</p>
             </div>
 
             <div className="box2">
-                <h2>Another Box Title</h2>
-                <p>Another box content...</p>
+                <h2>News</h2>
+                <p>News for today..</p>
+            </div>
+
+            <div className="box3">
+                <h2>Latest products in store</h2>
+                <p>Some really cool new products now in store!</p>
             </div>
         </>
     );
