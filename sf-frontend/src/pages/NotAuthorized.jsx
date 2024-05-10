e55724ac0530394bed7653cd9bbfa2e958dbfc1a
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-05-10 09:36:16 +0200

    Account routes protected with HOC help/HOC component

--- sf-frontend/src/pages/NotAuthorized.jsx
+++ sf-frontend/src/pages/NotAuthorized.jsx
@@ -5,7 +5,7 @@ function NotAuthorized() {
     return (
         <div className="notAuthenticated">
             <h1 className="sadFace">:(</h1>
-            <h2 className="message">I'm sorry you're not authenticated to do that, try logging in instead!</h2>
+            <h2 className="message">I'm sorry you're not authorized to do that, try logging in instead!</h2>
         </div>
     );
 }
