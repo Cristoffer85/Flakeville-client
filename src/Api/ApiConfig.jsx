6813122ca8189fcbe12d4890493b633d156f0080
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-12-27 19:01:53 +0100

    server url updated

--- src/Api/ApiConfig.jsx
+++ src/Api/ApiConfig.jsx
@@ -3,7 +3,7 @@
 const config = {
             // 
       // backendUrl: 'http://localhost:8080'      
-         backendUrl: 'https://flakeville-server.onrender.com' 
+         backendUrl: 'https://flakeville-server-2.onrender.com' 
 };
 
 export default config;
