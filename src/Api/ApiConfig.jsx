c6ecd66826cd02d336fa3acad98f66a4ae467169
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-12-30 23:12:49 +0100

    Backend api updated to server

--- src/Api/ApiConfig.jsx
+++ src/Api/ApiConfig.jsx
@@ -1,9 +1,8 @@
 /* One file to change the backend URL. So much time saver.. right? */
 
 const config = {
-            // 
-       backendUrl: 'http://localhost:8080'      
-      //   backendUrl: 'https://flakeville-server-2.onrender.com' 
+            // backendUrl: 'http://localhost:8080'      
+         backendUrl: 'https://flakeville-server-2.onrender.com' 
 };
 
 export default config;
