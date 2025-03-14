85cc7183d69729055ab627e3921e4762948b1699
Cristoffer <cristoffer_ostberg85@hotmail.com>
2025-03-14 11:48:15 +0100

    Another uppercase main2

--- src/MAIN2.jsx
+++ src/MAIN2.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
+import App from './APP2';
+import './INDEX2.css';
 import { AuthProvider } from './CONTEXTS2/AUTHCONTEXT2/AUTHCONTEXT2.js';
 
 const container = document.getElementById('root');
